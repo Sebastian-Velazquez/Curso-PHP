05ca77a8313e751d256ea55fd778e1308d4ad763
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-08-17 11:58:42 -0300

    first commit

---
+++ ejercicio1.php
@@ -0,0 +1,22 @@
+<?php 
+    $nombre =$_POST('txtNombre');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello word";//Forma de imprimir
+
+    print_r (" Hello word");//Forma de imprimir
+
+    print(" Hollo Word");//Forma de imprimir
+?>
+ 
+</body>
+</html>
+
